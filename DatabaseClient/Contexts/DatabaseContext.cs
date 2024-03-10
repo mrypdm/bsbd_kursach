52ee4ae140f52fa4b30db8debc679451a730a0e8
Maxim Shapovalov <mrypdm@gmail.com>
2024-03-10 14:42:31 +0300

    context and factory

---
+++ DatabaseClient/Contexts/DatabaseContext.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using DatabaseClient.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseClient.Contexts;
+
+public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
+{
+    // Tables
+
+    public DbSet<Book> Books { get; set; }
+
+    public DbSet<Client> Clients { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
+
+    public DbSet<OrdersToBook> OrdersToBooks { get; set; }
+
+    public DbSet<Review> Reviews { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    // Database configuration
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasIndex(e => e.Author, "IX_Books_Author");
+
+            entity.HasIndex(e => e.Title, "IX_Books_Title");
+
+            entity.Property(e => e.Author)
+                .IsRequired()
+                .HasMaxLength(50);
+            entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasIndex(e => e.Phone, "IX_Clients_Phone").IsUnique();
+
+            entity.Property(e => e.FirstName).IsRequired();
+            entity.Property(e => e.LastName).IsRequired();
+            entity.Property(e => e.Phone)
+                .IsRequired()
+                .HasMaxLength(10)
+                .IsFixedLength();
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasOne(d => d.Client).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.ClientId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_Orders_Clients");
+        });
+
+        modelBuilder.Entity<OrdersToBook>(entity =>
+        {
+            entity.HasKey(e => new { e.OrderId, e.BookId });
+
+            entity.HasOne(d => d.Book).WithMany(p => p.OrdersToBooks)
+                .HasForeignKey(d => d.BookId)
+                .HasConstraintName("FK_OrdersToBooks_Books");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrdersToBooks)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_OrdersToBooks_Orders");
+        });
+
+        modelBuilder.Entity<Review>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_Review_1");
+
+            entity.ToTable("Review");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.HasOne(d => d.Book).WithMany(p => p.Reviews)
+                .HasForeignKey(d => d.BookId)
+                .HasConstraintName("FK_Review_Books");
+
+            entity.HasOne(d => d.Client).WithMany(p => p.Reviews)
+                .HasForeignKey(d => d.ClientId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_Review_Clients");
+        });
+
+        modelBuilder.Entity<Tag>(entity =>
+        {
+            entity.HasIndex(e => e.Title, "IX_Tags_Title").IsUnique();
+
+            entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.HasMany(d => d.Books).WithMany(p => p.Tags)
+                .UsingEntity<Dictionary<string, object>>(
+                    "BooksToTag",
+                    r => r.HasOne<Book>().WithMany()
+                        .HasForeignKey("BookId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK_BooksToTags_Books"),
+                    l => l.HasOne<Tag>().WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_BooksToTags_Tags"),
+                    j =>
+                    {
+                        j.HasKey("TagId", "BookId");
+                        j.ToTable("BooksToTags");
+                    });
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
