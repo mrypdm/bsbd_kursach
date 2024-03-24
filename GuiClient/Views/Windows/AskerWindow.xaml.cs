606c9af482d4ff2ef18a9a44e074a12aa1b745c7
Maxim Shapovalov <mrypdm@gmail.com>
2024-03-24 16:12:43 +0300

    asker window

---
+++ GuiClient/Views/Windows/AskerWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+
+namespace GuiClient.Views.Windows;
+
+public partial class AskerWindow : Window
+{
+    private AskerWindow(string message, string initialValue = "")
+    {
+        InitializeComponent();
+
+        MessageBlock.Text = message;
+        ValueBox.Text = initialValue;
+    }
+
+    private void Ok(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        Close();
+    }
+
+    private void Cancel(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
+    }
+
+    public static string AskString(string message, string initialValue = "")
+    {
+        var window = new AskerWindow(message, initialValue);
+        return window.ShowDialog() == true ? window.ValueBox.Text : null;
+    }
+
+    public static int? AskInt(string message, string initialValue = "")
+    {
+        while (true)
+        {
+            var input = AskString(message, initialValue);
+
+            if (input == null)
+            {
+                return null;
+            }
+
+            if (int.TryParse(input, out var count))
+            {
+                return count;
+            }
+
+            MessageBox.Show("Invalid value", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
