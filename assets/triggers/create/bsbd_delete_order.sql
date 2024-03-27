b13a81965fe5e6a1f22be7e3fd893b2d00fd141d
Maxim Shapovalov <mrypdm@gmail.com>
2024-03-27 09:17:34 +0300

    better trigger for delete order

--- assets/triggers/create/bsbd_delete_order.sql
+++ assets/triggers/create/bsbd_delete_order.sql
@@ -1,13 +1,7 @@
 create trigger bsbd_delete_order on OrdersToBooks after delete as
 begin
-    declare @currentOrdersCount int
-    -- if order is deleted then there is no order in Orders table
-    -- then join return empty result => count = 0
-    -- if order is not deleted but some of the books does then order is in Orders table
-    -- then join return not empty resul => count = 1
-    set @currentOrdersCount = (select count(distinct o.Id) from Orders o join deleted d on o.Id = d.OrderId)
-
-    if @currentOrdersCount != 0 begin
+    if exists(select Id from Orders intersect select OrderId as Id from deleted)
+    begin
         raiserror('Cannot delete book from order, because order is not deleted',15,1)
         rollback transaction
     end
