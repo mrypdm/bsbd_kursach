9b89d9d6d3d88073f0c0247735469e8b24d582e4
Maxim Shapovalov <mrypdm@gmail.com>
2024-03-27 21:35:19 +0300

    from tag to books

--- GuiClient/ViewModels/Windows/AllTagsViewModel.cs
+++ GuiClient/ViewModels/Windows/AllTagsViewModel.cs
@@ -1,18 +1,34 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 using AutoMapper;
+using DatabaseClient.Extensions;
 using DatabaseClient.Models;
 using DatabaseClient.Repositories.Abstraction;
+using GuiClient.Commands;
 using GuiClient.Contexts;
+using GuiClient.Dto;
 using GuiClient.ViewModels.Abstraction;
 using GuiClient.Views.Windows;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GuiClient.ViewModels.Windows;
 
-public class AllTagsViewModel(ISecurityContext securityContext, ITagsRepository tagsRepository, IMapper mapper)
-    : AllEntitiesViewModel<Tag, Tag>(securityContext, tagsRepository, mapper)
+public class AllTagsViewModel : AllEntitiesViewModel<Tag, Tag>
 {
+    private readonly ITagsRepository _tagsRepository;
+
+    public AllTagsViewModel(ISecurityContext securityContext, ITagsRepository tagsRepository, IMapper mapper)
+        : base(securityContext, tagsRepository, mapper)
+    {
+        _tagsRepository = tagsRepository;
+
+        ShowBooks = new AsyncFuncCommand<Tag>(ShowBooksAsync);
+    }
+
+    public ICommand ShowBooks { get; }
+
     public override void EnrichDataGrid(AllEntitiesWindow window)
     {
         base.EnrichDataGrid(window);
@@ -20,6 +36,7 @@ public class AllTagsViewModel(ISecurityContext securityContext, ITagsRepository 
         if (IsWorker)
         {
             AddButton(window, "Update", nameof(Update));
+            AddButton(window, "Show books", nameof(ShowBooks));
         }
 
         AddText(window, nameof(Tag.Id), true);
@@ -30,14 +47,31 @@ public class AllTagsViewModel(ISecurityContext securityContext, ITagsRepository 
     {
         if (item.Id == -1)
         {
-            var tag = await tagsRepository.AddTagAsync(item.Name);
+            var tag = await _tagsRepository.AddTagAsync(item.Name);
             MessageBox.Show($"Tag created with ID={tag.Id}");
         }
         else
         {
-            await tagsRepository.UpdateAsync(item);
+            await _tagsRepository.UpdateAsync(item);
         }
 
         await RefreshAsync();
+    }
+
+    private async Task ShowBooksAsync(Tag item)
+    {
+        var allReviews = App.ServiceProvider.GetRequiredService<IEntityViewModel<Book, BookDto>>();
+
+        await allReviews.ShowBy(
+            r =>
+            {
+                var repo = r.Cast<Book, IBooksRepository>();
+                return repo.GetBooksByTagsAsync([item.Name]);
+            },
+            () => Task.FromResult(new BookDto
+            {
+                Id = -1,
+                Tags = item.Name
+            }));
     }
 }
