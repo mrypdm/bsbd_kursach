8fc733b6449e3af1c93037f7100d31e62fba94f2
Maxim Shapovalov <mrypdm@gmail.com>
2024-03-31 15:58:09 +0300

    reviews to raw sql

---
+++ DatabaseClient/Models/Internal/DbReview.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DatabaseClient.Models.Internal;
+
+[Serializable]
+public class DbReview : IDbEntity<Review>
+{
+    public int BookId { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public DateTime ReleaseDate { get; set; }
+
+    public int Price { get; set; }
+
+    public int Count { get; set; }
+
+    public bool IsBookDeleted { get; set; }
+
+    public int ClientId { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Phone { get; set; }
+
+    public Gender Gender { get; set; }
+
+    public bool IsClientDeleted { get; set; }
+
+    public int Score { get; set; }
+
+    public string Text { get; set; }
+
+    public Review ToEntity()
+    {
+        return new Review
+        {
+            Score = Score,
+            Text = Text,
+            BookId = BookId,
+            Book = new Book
+            {
+                Id = BookId,
+                Title = Title,
+                Author = Author,
+                ReleaseDate = ReleaseDate,
+                Count = Count,
+                Price = Price,
+                IsDeleted = IsBookDeleted
+            },
+            ClientId = ClientId,
+            Client = new Client
+            {
+                Id = ClientId,
+                FirstName = FirstName,
+                LastName = LastName,
+                Phone = Phone,
+                Gender = Gender,
+                IsDeleted = IsClientDeleted
+            }
+        };
+    }
+}
--- DatabaseClient/Repositories/ReviewsRepository.cs
+++ DatabaseClient/Repositories/ReviewsRepository.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DatabaseClient.Contexts;
+using DatabaseClient.Extensions;
 using DatabaseClient.Models;
+using DatabaseClient.Models.Internal;
 using DatabaseClient.Repositories.Abstraction;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,11 +17,25 @@ public class ReviewsRepository(DatabaseContextFactory factory) : IReviewsReposit
         ArgumentNullException.ThrowIfNull(client);
 
         await using var context = factory.Create();
-        return await context.Reviews
-            .Where(m => m.ClientId == client.Id)
-            .Include(m => m.Book)
-            .Include(m => m.Client)
-            .ToListAsync();
+
+        // return await context.Reviews
+        //     .Where(m => m.ClientId == client.Id)
+        //     .Include(m => m.Book)
+        //     .Include(m => m.Client)
+        //     .ToListAsync();
+
+        return await context.Database
+            .SqlQuery<DbReview>(
+                $"""
+                 select r.BookId, r.ClientId, r.Score, r.Text,
+                        b.Title, b.Author, b.ReleaseDate, b.Count, b.Price, b.IsDeleted as IsBookDeleted,
+                        c.FirstName, c.LastName, c.Phone, c.Gender, c.IsDeleted as IsClientDeleted
+                 from Reviews r
+                 join Clients c on c.Id = r.ClientId
+                 join Books b on b.Id = r.BookId
+                 where c.Id = {client.Id}
+                 """)
+            .AsListAsync<DbReview, Review>();
     }
 
     public async Task<ICollection<Review>> GetReviewForBooksAsync(Book book)
@@ -28,11 +43,25 @@ public class ReviewsRepository(DatabaseContextFactory factory) : IReviewsReposit
         ArgumentNullException.ThrowIfNull(book);
 
         await using var context = factory.Create();
-        return await context.Reviews
-            .Where(m => m.BookId == book.Id)
-            .Include(m => m.Book)
-            .Include(m => m.Client)
-            .ToListAsync();
+
+        // return await context.Reviews
+        //     .Where(m => m.BookId == book.Id)
+        //     .Include(m => m.Book)
+        //     .Include(m => m.Client)
+        //     .ToListAsync();
+
+        return await context.Database
+            .SqlQuery<DbReview>(
+                $"""
+                 select r.BookId, r.ClientId, r.Score, r.Text,
+                        b.Title, b.Author, b.ReleaseDate, b.Count, b.Price, b.IsDeleted as IsBookDeleted,
+                        c.FirstName, c.LastName, c.Phone, c.Gender, c.IsDeleted as IsClientDeleted
+                 from Reviews r
+                 join Clients c on c.Id = r.ClientId
+                 join Books b on b.Id = r.BookId
+                 where b.Id = {book.Id}
+                 """)
+            .AsListAsync<DbReview, Review>();
     }
 
     public async Task<Review> AddReviewAsync(Client client, Book book, int score, string text = null)
@@ -40,26 +69,57 @@ public class ReviewsRepository(DatabaseContextFactory factory) : IReviewsReposit
         ArgumentNullException.ThrowIfNull(client);
         ArgumentNullException.ThrowIfNull(book);
 
-        var review = new Review
-        {
-            ClientId = client.Id,
-            BookId = book.Id,
-            Score = score,
-            Text = text
-        };
-
         await using var context = factory.Create();
-        var entity = await context.Reviews.AddAsync(review);
-        await context.SaveChangesAsync();
-        return entity.Entity;
+
+        // var review = new Review
+        // {
+        //     ClientId = client.Id,
+        //     BookId = book.Id,
+        //     Score = score,
+        //     Text = text
+        // };
+        //
+        // var entity = await context.Reviews.AddAsync(review);
+        // await context.SaveChangesAsync();
+        // return entity.Entity;
+
+        return await context.Database
+            .SqlQuery<DbReview>(
+                $"""
+                 insert into Reviews (ClientId, BookId, Score, Text)
+                 values ({client.Id}, {book.Id}, {score}, {text})
+
+                 select r.BookId, r.ClientId, r.Score, r.Text,
+                        b.Title, b.Author, b.ReleaseDate, b.Count, b.Price, b.IsDeleted as IsBookDeleted,
+                        c.FirstName, c.LastName, c.Phone, c.Gender, c.IsDeleted as IsClientDeleted
+                 from Reviews r
+                 join Clients c on c.Id = r.ClientId
+                 join Books b on b.Id = r.BookId
+                 where c.Id = {client.Id} and b.Id = {book.Id}
+                 """)
+            .SingleOrDefaultAsync<DbReview, Review>();
     }
 
     public async Task<Review> GetByIdAsync(int bookId, int clientId)
     {
         await using var context = factory.Create();
-        return await context.Reviews
-            .Where(m => m.BookId == bookId && m.ClientId == clientId)
-            .SingleOrDefaultAsync();
+
+        // return await context.Reviews
+        //     .Where(m => m.BookId == bookId && m.ClientId == clientId)
+        //     .SingleOrDefaultAsync();
+
+        return await context.Database
+            .SqlQuery<DbReview>(
+                $"""
+                 select r.BookId, r.ClientId, r.Score, r.Text,
+                        b.Title, b.Author, b.ReleaseDate, b.Count, b.Price, b.IsDeleted as IsBookDeleted,
+                        c.FirstName, c.LastName, c.Phone, c.Gender, c.IsDeleted as IsClientDeleted
+                 from Reviews r
+                 join Clients c on c.Id = r.ClientId
+                 join Books b on b.Id = r.BookId
+                 where c.Id = {clientId} and b.Id = {bookId}
+                 """)
+            .SingleOrDefaultAsync<DbReview, Review>();
     }
 
     public Task<Review> GetByIdAsync(int id)
@@ -70,20 +130,43 @@ public class ReviewsRepository(DatabaseContextFactory factory) : IReviewsReposit
     public async Task<ICollection<Review>> GetAllAsync()
     {
         await using var context = factory.Create();
-        return await context.Reviews
-            .Include(m => m.Book)
-            .Include(m => m.Client)
-            .ToArrayAsync();
+
+        // return await context.Reviews
+        //     .Include(m => m.Book)
+        //     .Include(m => m.Client)
+        //     .ToArrayAsync();
+
+        return await context.Database
+            .SqlQuery<DbReview>(
+                $"""
+                 select r.BookId, r.ClientId, r.Score, r.Text,
+                        b.Title, b.Author, b.ReleaseDate, b.Count, b.Price, b.IsDeleted as IsBookDeleted,
+                        c.FirstName, c.LastName, c.Phone, c.Gender, c.IsDeleted as IsClientDeleted
+                 from Reviews r
+                 join Clients c on c.Id = r.ClientId
+                 join Books b on b.Id = r.BookId
+                 """)
+            .AsListAsync<DbReview, Review>();
     }
 
     public async Task UpdateAsync(Review entity)
     {
+        ArgumentNullException.ThrowIfNull(entity);
+
         await using var context = factory.Create();
-        await context.Reviews
-            .Where(m => m.BookId == entity.BookId && m.ClientId == entity.ClientId)
-            .ExecuteUpdateAsync(o => o
-                .SetProperty(m => m.Text, entity.Text)
-                .SetProperty(m => m.Score, entity.Score));
+
+        // await context.Reviews
+        //     .Where(m => m.BookId == entity.BookId && m.ClientId == entity.ClientId)
+        //     .ExecuteUpdateAsync(o => o
+        //         .SetProperty(m => m.Text, entity.Text)
+        //         .SetProperty(m => m.Score, entity.Score));
+
+        await context.Database.ExecuteSqlAsync(
+            $"""
+             update Reviews
+             set Score = {entity.Score}, Text = {entity.Text}
+             where ClientId = {entity.ClientId} and BookId = {entity.BookId}
+             """);
     }
 
     public async Task RemoveAsync(Review entity)
@@ -94,8 +177,7 @@ public class ReviewsRepository(DatabaseContextFactory factory) : IReviewsReposit
         }
 
         await using var context = factory.Create();
-        await context.Reviews
-            .Where(m => m.BookId == entity.BookId && m.ClientId == entity.ClientId)
-            .ExecuteDeleteAsync();
+        await context.Database.ExecuteSqlAsync(
+            $"delete from Reviews where BookId = {entity.BookId} and ClientId = {entity.ClientId}");
     }
 }
