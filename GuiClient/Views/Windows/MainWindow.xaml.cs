d0def1762d76c57899caf24a666db1a98a11ff17
Maxim Shapovalov <mrypdm@gmail.com>
2024-04-02 08:19:48 +0300

    remove unused

--- GuiClient/Views/Windows/MainWindow.xaml.cs
+++ GuiClient/Views/Windows/MainWindow.xaml.cs
@@ -1,13 +1,11 @@
 using System.Windows;
 using GuiClient.ViewModels.Windows;
-using JetBrains.Annotations;
 
 namespace GuiClient.Views.Windows;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-[UsedImplicitly]
 public partial class MainWindow : Window
 {
     public MainWindow()
